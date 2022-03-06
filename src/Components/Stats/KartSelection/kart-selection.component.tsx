7e74116ef81102bc1d905d1276d7894cbdea4885
jjh47e <62762383+JJH47E@users.noreply.github.com>
2022-03-06 13:52:42 +0000

    fix bug

--- src/Components/Stats/KartSelection/kart-selection.component.tsx
+++ src/Components/Stats/KartSelection/kart-selection.component.tsx
@@ -1,42 +1,26 @@
-import React, { useEffect, useRef } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Button from '../../Shared/button.component';
 import VehicleClass from '../../../Models/vehicle-class.model';
 import VehicleData from '../../../data/class-vehicles.json';
 import '../../../Root.scss';
-import CharacterDetail from '../../../Models/character-detail.model';
-import KartStatSelection from '../../../Models/kart-stat-selection.model';
-import PlayerCombo from '../../../Models/player-combo.model';
-import { getOtherRegion, switchRegion } from '../../../Services/region.service';
 import { getRegionalVariant } from '../../../Services/vehicle-mapper.service';
 import RegionSwitch from '../../Shared/region-switch.component';
 import { globalGetCharacter } from '../../../Services/character-selection.service';
 import { getCharacterClass } from '../../../Services/chartacter-stats.service';
+import { globalSetKart } from '../../../Services/kart-selection.service';
 
 const vehicleData = VehicleData as VehicleClass[];
 
-const mouseOverStart = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
-  const { style } = event.target as HTMLElement;
-  style.textDecoration = 'underline';
-};
-
-const mouseOverEnd = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
-  const { style } = event.target as HTMLElement;
-  style.textDecoration = 'none';
-};
-
 function KartSelection() {
   const navigate = useNavigate();
-
-  const changeRegion = () => {
-    switchRegion();
-  };
 
   const homePage = () => {
     navigate('/mkwii-combo-gen/', { replace: false });
   };
 
-  function onKartSelectFn(character: CharacterDetail, kart: string) {
+  function onKartSelectFn(kart: string) {
+    globalSetKart(kart);
     window.scrollTo(0, 0);
     navigate('/mkwii-combo-gen/stats/summary', {
       replace: false,
@@ -60,7 +44,6 @@ function KartSelection() {
   }
 
   const characterClass = getCharacterClass(character);
-  const characterDetail = { name: character, class: characterClass };
 
   const karts = vehicleData.find(v => v.class === characterClass)
     ?.vehicles as string[];
@@ -76,7 +59,7 @@ function KartSelection() {
               <Button
                 key={vehicle}
                 buttonText={getRegionalVariant(vehicle)}
-                onClick={() => onKartSelectFn(characterDetail, vehicle)}
+                onClick={() => onKartSelectFn(vehicle)}
               />
             );
           })}
